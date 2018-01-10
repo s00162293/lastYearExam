a61c1f93268a085a15f61824da8f6ed12767c552
rubabramzan96 <rubabramzan96@gmail.com>
2018-01-10 14:46:18 +0000

    Add project files.

---
+++ WebApplicationSAmpleExam/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationSAmpleExam.MvcApplication" Language="C#" %>
---
+++ WebApplicationSAmpleExam/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationSAmpleExam.Startup))]
+namespace WebApplicationSAmpleExam
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
